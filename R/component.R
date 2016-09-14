24b61640cbb9910867f6ae02f864800552eb1c19
Nokome Bentley <nokome@stenci.la>
2016-09-14 15:23:56 +1200

    Implement component read/write methods

--- R/component.R
+++ R/component.R
@@ -1,4 +1,63 @@
 Component <- R6Class("Component",
   public = list(
+
+    initialize = function (path = NULL) {
+      path <- if (!is.null(path)) path else ''
+
+      private$.path <- path
+      if (file.exists(path)) {
+        self$read(path)
+      }
+
+      count <- components$count + 1
+      assign(paste0('.', count), self, envir = components)
+      assign('count', count, envir = components)
+
+    },
+
+    path = function () {
+      private$.path
+    },
+
+    read = function (path = NULL) {
+      if (is.null(path) || path == '') {
+        path <- private$.path
+      }
+
+      if (file.exists(path)) {
+        private$.path <- path
+      } else {
+        stop("Filesystem path does not exist\n  path: ${path}")
+      }
+
+      private$.path
+    },
+
+    write  = function (path = NULL) {
+      if (is.null(path) || path == '') {
+        path <- private$.path
+      }
+
+      dir <- if (tools::file_ext(path) == '') path else dirname(path)
+      if (!dir.exists(dir)) {
+        dir.create(dir, recursive = TRUE)
+      }
+
+      private$.path <- path
+
+      private$.path
+    }
+
+  ),
+
+  private = list(
+    .path = NULL
   )
 )
+
+# List of component instances
+# Needs to be an environment to avoid the
+# "cannot change value of locked binding" error
+# when trying to change a package variable
+components <- new.env()
+assign('count', 0, envir = components)
--- tests/testthat/test.component.R
+++ tests/testthat/test.component.R
@@ -3,3 +3,48 @@ test_that("Component an be constructed", {
 
   expect_equal(class(c)[1], "Component")
 })
+
+test_that('Component can be read and written', {
+  p1 = paste0(tempfile(), '.tmp')
+  p2 = paste0(tempfile(), '.tmp')
+  # R doesn't actually creates the files, so do that
+  cat('', file = p1)
+  cat('', file = p2)
+
+  c = Component$new(p1);
+  expect_equal(c$path(), p1, 'Initial path is set')
+
+  c$read()
+  expect_equal(c$path(), p1, 'Read with no arg does not change path')
+
+  c$read(p2)
+  expect_equal(c$path(), p2, 'Read with arg does change path')
+
+  c$write()
+  expect_equal(c$path(), p2, 'Write with no arg does not change path')
+
+  c$write(p1)
+  expect_equal(c$path(), p1, 'Write with arg does change path')
+})
+
+test_that('Component read errors correctly', {
+  c = Component$new('foo/bar');
+  expect_equal(c$path(), 'foo/bar', 'Initial path is set and doesnt matter if it doesnt exist')
+  #expect_equal(throws(c$read.bind(c), /Filesystem path does not exist/, 'Explicit read should error')
+});
+
+test_that('Component writer creates the right directories', {
+  dir = tempdir()
+
+  file = file.path(dir, 'bar', 'boop.txt')
+  c = Component$new(file)
+  expect_equal(c$path(), file, 'Path is set')
+  c$write()
+  expect_true(dir.exists(file.path(dir, 'bar')), 'Parent directory is created')
+  expect_false(file.exists(file), 'File is not written')
+
+  dir2 = file.path(dir, 'bee')
+  c$write(dir2)
+  expect_equal(c$path(), dir2, 'Path is set')
+  expect_true(dir.exists(dir2), 'Directory is written')
+});
