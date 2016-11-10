c1a3d31cf27e9aec6c49e311bf327eb8547417fc
Nokome Bentley <nokome@stenci.la>
2016-11-10 17:23:20 +1300

    Detect local requests in ComponentProxy

--- R/component-proxy.R
+++ R/component-proxy.R
@@ -11,52 +11,78 @@ ComponentProxy <- function(type, url) {
   self$.type <- type
   self$.url <- url
 
+  # There is some sort of conflict between `httr` and `httpuv` packages
+  # such that `httr` timeouts when making a request to the same port as
+  # this host is listening on. Usually such a self-request will only be done
+  # occasionally (e.g. during testing) . But to prevent these timeouts we bypass
+  # making self-requests by checking to see if this component is actually local to this process
+  com_parts <- parse_url(url)
+  host_parts <- parse_url(host$url)
+  if (com_parts$hostname == host_parts$hostname && com_parts$port == host_parts$port) {
+    self$.local <- TRUE
+    self$.address <- com_parts$path
+  } else {
+    self$.local <- FALSE
+  }
+
   self$.get <- function(name) {
-    response <- GET(
-      paste0(self[['.url']], '!', name),
-      accept_json(),
-      timeout(10)
-    )
-    code <- response$status_code
-    text <- content(response, 'text', encoding = 'UTF-8')
-    if (code == 200 && nchar(text) > 0) {
-      fromJSON(text)
+    if (self$.local) {
+      host$open(self$.address)[[name]]
     } else {
-      stop(paste0('Error in proxy get\n  code: ', code, '\n  message: ',text))
+      response <- GET(
+        paste0(self[['.url']], '!', name),
+        accept_json(),
+        timeout(10)
+      )
+      code <- response$status_code
+      text <- content(response, 'text', encoding = 'UTF-8')
+      if (code == 200 && nchar(text) > 0) {
+        fromJSON(text)
+      } else {
+        stop(paste0('Error in proxy get\n  code: ', code, '\n  message: ',text))
+      }
     }
   }
 
   self$.set <- function(name, value) {
-    response <- PUT(
-      paste0(self[['.url']], '!', name),
-      accept_json(),
-      content_type_json(),
-      timeout(10),
-      body = toJSON(value, auto_unbox = TRUE)
-    )
-    text <- content(response, 'text', encoding = 'UTF-8')
-    if (response$status_code != 200) {
-      stop(paste0('Error in proxy set:', text))
+    if (self$.local) {
+      host$open(self$.address)[[name]] <- value
+    } else {
+        response <- PUT(
+        paste0(self[['.url']], '!', name),
+        accept_json(),
+        content_type_json(),
+        timeout(10),
+        body = toJSON(value, auto_unbox = TRUE)
+      )
+      text <- content(response, 'text', encoding = 'UTF-8')
+      if (response$status_code != 200) {
+        stop(paste0('Error in proxy set:', text))
+      }
     }
   }
 
   self$.call <- function(name, ...) {
-    response <- POST(
-      paste0(self[['.url']], '!', name),
-      accept_json(),
-      content_type_json(),
-      timeout(10),
-      body = toJSON(list(...), auto_unbox = TRUE)
-    )
-    text <- content(response, 'text', encoding = 'UTF-8')
-    if (response$status_code == 200) {
-      if (nchar(text) > 0) {
-        fromJSON(text)
-      } else {
-        invisible()
-      }
+    if (self$.local) {
+      host$open(self$.address)[[name]](...)
     } else {
-      stop(paste0('Error in proxy call:', text))
+      response <- POST(
+        paste0(self[['.url']], '!', name),
+        accept_json(),
+        content_type_json(),
+        timeout(10),
+        body = toJSON(list(...), auto_unbox = TRUE)
+      )
+      text <- content(response, 'text', encoding = 'UTF-8')
+      if (response$status_code == 200) {
+        if (nchar(text) > 0) {
+          fromJSON(text)
+        } else {
+          invisible()
+        }
+      } else {
+        stop(paste0('Error in proxy call:', text))
+      }
     }
   }
 
