4cf6088c497af3d8b444b0cbbc73df39890c49bb
Nokome Bentley <nokome@stenci.la>
2018-01-29 16:45:17 +1300

    Add in to register a function

--- R/addins.R
+++ R/addins.R
@@ -11,3 +11,17 @@ addin_open <- function() {
   # Open it!
   host$open(paste0('file://', path))
 }
+
+#' Register the function in the source editor that is currently open
+addin_register <- function() {
+  # Get the path of the file currently in the source editor
+  editor <- rstudioapi::getSourceEditorContext()
+  path <- editor$path
+  # If path is empty (as it is for a new document) we could revert to loading the
+  # editor$contents but currently error
+  if (nchar(path)==0) stop('Error when attempting to open in Stencila: no file currently active', call.=FALSE)
+  # Normalise the file path
+  path <- normalizePath(path, winslash = '/')
+  # Open it!
+  function_register(path)
+}
